cc8aa1e61ae0be4e81d4db31c38bd674de0f4046
Scott Crowther <39570463+snc-software@users.noreply.github.com>
2026-09-12 09:13:58 +0100

    initial project setup

---
+++ WorkoutTracker/WorkoutTrackerTests/WorkoutTrackerTests.swift
@@ -0,0 +1,19 @@
+//
+//  WorkoutTrackerTests.swift
+//  WorkoutTrackerTests
+//
+//  Created by Scott Crowther on 12/09/2026.
+//
+
+import Testing
+@testable import WorkoutTracker
+
+struct WorkoutTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
